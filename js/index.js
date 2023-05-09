38bc34fea530aed39735958b2ed23bcd4cf6f7e5
deshwaninikil <79966144+deshwaninikil@users.noreply.github.com>
2023-05-10 01:44:53 +0530

    change some css

--- js/index.js
+++ js/index.js
@@ -1,99 +1,98 @@
 const videos = [
   {
+    id: 1,
     title: "The World's Most Expensive Cruise Ship",
     youtubeLink: "https://www.youtube.com/embed/tgbNymZ7vqY",
     publishDate: "2022-04-01",
   },
   {
+    id: 2,
     title: "How to Train Your Brain to Learn Faster and Remember More",
     youtubeLink: "https://www.youtube.com/embed/7Mgb7Xt_2Qw",
     publishDate: "2022-04-10",
   },
   {
+    id: 3,
     title: "What If We Nuke a City?",
     youtubeLink: "https://www.youtube.com/embed/isFkFWxu21c",
     publishDate: "2022-04-15",
   },
   {
+    id: 4,
     title: "The Mystery of the Bermuda Triangle",
     youtubeLink: "https://www.youtube.com/embed/AGBjI0x9VbM",
     publishDate: "2022-04-20",
   },
   {
+    id: 5,
     title: "What If Earth Had Rings Like Saturn?",
     youtubeLink: "https://www.youtube.com/embed/Jj5tc8T84Q8",
     publishDate: "2022-05-01",
   },
   {
+    id: 6,
     title: "How to Build a Sustainable Future for Our Planet",
     youtubeLink: "https://www.youtube.com/embed/mK9J8ByjT4U",
     publishDate: "2022-05-05",
   },
   {
+    id: 7,
     title: "The Physics of Roller Coasters",
     youtubeLink: "https://www.youtube.com/embed/e-AEP3EH69Y",
     publishDate: "2022-05-11",
   },
   {
+    id: 8,
     title: "Why Are We Afraid of Spiders?",
     youtubeLink: "https://www.youtube.com/embed/Rd_fIWhGfK4",
     publishDate: "2022-05-16",
   },
   {
+    id: 9,
     title: "The Science of Laughter",
     youtubeLink: "https://www.youtube.com/embed/LmzMXtKI5UQ",
     publishDate: "2022-05-20",
   },
   {
+    id: 10,
     title: "The Power of Positivity: How to Train Your Brain to Be Happy",
     youtubeLink: "https://www.youtube.com/embed/-L1QON_ol7c",
     publishDate: "2022-05-25",
   },
   {
+    id: 11,
     title:
       "The Art of Storytelling: Pixar's 22 Rules for Phenomenal Storytelling",
     youtubeLink: "https://www.youtube.com/embed/g11JjVF618g",
     publishDate: "2022-06-01",
   },
   {
+    id: 12,
+
     title: "The Secrets of the Universe: The Big Bang Theory Explained",
     youtubeLink: "https://www.youtube.com/embed/ivag0CfOcNc",
     publishDate: "2022-06-05",
   },
   {
+    id: 13,
     title: "The Evolution of Music: From Prehistoric Sounds to Modern-Day Hits",
     youtubeLink: "https://www.youtube.com/embed/7lDpCPsLcQg",
     publishDate: "2022-06-10",
   },
   {
+    id: 14,
     title:
       "How to Make a Fortune on the Stock Market: Tips from a Wall Street Insider",
     youtubeLink: "https://www.youtube.com/embed/bCtlituS9p8",
     publishDate: "2022-06-15",
   },
   {
+    id: 15,
     title: "The Science of Dreams: What Do They Mean and Why Do We Have Them?",
     youtubeLink: "https://www.youtube.com/embed/X7R-q9rsrtU",
     publishDate: "2022-06-20",
   },
 ];
-
-var currentVideo = null;
-var iframes = document.getElementsByTagName("iframe");
-
-for (var i = 0; i < iframes.length; i++) {
-  iframes[i].addEventListener("click", function () {
-    // Pause any currently playing video
-    if (currentVideo != null && currentVideo != this) {
-      currentVideo.contentWindow.postMessage(
-        '{"event":"command","func":"pauseVideo","args":""}',
-        "*"
-      );
-    }
-
-    currentVideo = this;
-  });
-}
 
 function generateVideoHTML(video) {
   return `
@@ -118,7 +117,7 @@ function renderVideoPage(startIndex, pageSize, videos) {
 
 function renderPagination(startIndex, pageSize, videos) {
   let paginationContainer = document.getElementById("page-numbers");
-  paginationContainer.innerHTML = ""; // clear previous pagination
+  paginationContainer.innerHTML = "";
 
   let totalPages = Math.ceil(videos.length / pageSize);
   let currentPage = Math.floor(startIndex / pageSize) + 1;
